7e817137f7649ef73514bc2d9572a5bf1d432526
yort <k.kanchanapiboon@gmail.com>
2021-10-17 19:12:02 +0700

    finished error

---
+++ src/instruction.rs
@@ -0,0 +1,30 @@
+use std::convert::TryInto;
+use solana_program::program_error::ProgramError;
+
+pub enum EscrowInstrtuction {
+    InitEscrow {
+        amount: u64
+    }
+}
+
+impl EscrowInstrtuction {
+     pub fn unpack(input: &[u8]) -> Result<Self, ProgramError> {
+         let (tag, rest) = input.split_first().ok_or(InvalidInstruction)?;
+
+         Ok(match tag {
+             0 => Self::InitEscrow {
+                 amount: Self::unpack_amount(rest)?,
+             },
+             _ => return Err(InvalidInstruction.into()),
+         })
+     }
+
+    fn unpack_amount(input: &[u8]) -> Result<u64, ProgramError> {
+        let amount = input
+            .get(..8)
+            .and_then(|slice| slice.try_into().ok())
+            .map(u64::from_le_bytes)
+            .ok_or(InvalidInstruction)?;
+        Ok(amount)
+    }
+}
